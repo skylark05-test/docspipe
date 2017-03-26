ed0f830d9fed01e771aefe7dc5515b32ae32e96b
Ragesh Krishna <ragesh@rageshkrishna.com>
2017-03-26 19:18:11 +0530

    Foo

--- test.sh
+++ test.sh
@@ -1,3 +1,11 @@
 #!/bin/bash -e
 
+export DOCS_CONTEXT=lark
+
+export RES_AWS_CREDS="sky"$DOCS_CONTEXT"_aws"
+export RES_AWS_CREDS_UP=$(echo $RES_AWS_CREDS | awk '{print toupper($0)}')
+
+export AWS_ACCESS_KEY=$(eval echo "$"$RES_AWS_CREDS_UP"_INTEGRATION_AWS_ACCESS_KEY_ID")
+export AWS_SECRET_KEY=$(eval echo "$"$RES_AWS_CREDS_UP"_INTEGRATION_AWS_SECRET_ACCESS_KEY")
+
 env
